c24859c0159571a466ff543c67d5b1b4dbf6ce27
EvgVN <kolombo@bk.ru>
2023-10-17 00:32:49 +0400

    1. Убрать в классе Util поле instance и убрать конструктор и метод getInstance, класс Util не должен порождать обьекты, он должен содержать только static методы. Создать отдельный метод для получения connection 2. В классе сервиса нарушена инкапсуляция 3. Connection должен быть закрыт после всех операций в класcе Main. Для это пропиши метод закрытия в классе Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,8 +7,7 @@ import java.sql.SQLException;
 public class Util {
     // реализуйте настройку соеденения с БД
     private static Connection conn = null;
-    private static Util instance = null;
-    private Util() {
+    public static Connection getConnection() {
         try {
             if (null == conn || conn.isClosed()) {
                 conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/myDB", "root", "1");
@@ -16,14 +15,16 @@ public class Util {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-    public static Util getInstance() {
-        if (null == instance) {
-            instance = new Util();
-        }
-        return instance;
-    }
-    public Connection getConnection() {
         return conn;
+    }
+    public static void closeConnection() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+                System.out.println("Connection closed successfully");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
