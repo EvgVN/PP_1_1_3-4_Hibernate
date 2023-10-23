a8e1331433d9b7022c9ca43d346528c54be98ff8
EvgVN <kolombo@bk.ru>
2023-10-24 03:56:06 +0400

    1. Зачем в методе saveUser при  сохранении пользоваться PreparedStatement? Убрать и использовать метод save обьекта session 2. В методе getAllUsers не использвать CriteriaQuery 3. Во всех методах, если есть транзакция, то у нее должен быть предусмотрен rollback

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -27,84 +27,103 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
+            transaction = session.beginTransaction();
             session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), lastName VARCHAR(255), age INT)").executeUpdate();
             transaction.commit();
+            System.out.println("Table created successfully");
         } catch (HibernateException e) {
             e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
     public void dropUsersTable() {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
+            transaction = session.beginTransaction();
             session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
             transaction.commit();
+            System.out.println("Table dropped successfully");
         } catch (HibernateException e) {
             e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
-@Override
-public void saveUser(String name, String lastName, byte age) {
-    try (Session session = sessionFactory.openSession()) {
-        Transaction transaction = session.beginTransaction();
-        session.doWork(connection -> {
-            try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)")) {
-                preparedStatement.setString(1, name);
-                preparedStatement.setString(2, lastName);
-                preparedStatement.setByte(3, age);
-                preparedStatement.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            transaction.commit();
+            System.out.println("User with id " + user.getId() + " saved successfully");
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
             }
-        });
-        transaction.commit();
-    } catch (HibernateException e) {
-        e.printStackTrace();
+        }
     }
-}
 
+    @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
         SessionFactory sessionFactory = getSessionFactory();
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
+            transaction = session.beginTransaction();
             User user = session.load(User.class, id);
             session.remove(user);
             transaction.commit();
             System.out.println("User with id " + id + " deleted successfully");
         } catch (HibernateException e) {
             e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
     public List<User> getAllUsers() {
+        Transaction transaction = null;
         List<User> users = new ArrayList<>();
         SessionFactory sessionFactory = getSessionFactory();
         try (Session session = sessionFactory.openSession()) {
-            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-            CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
-            Root<User> root = criteriaQuery.from(User.class);
-            criteriaQuery.select(root);
-            Query<User> query = session.createQuery(criteriaQuery);
+            transaction = session.beginTransaction();
+            Query<User> query = session.createQuery("FROM User", User.class);
             users = query.getResultList();
+            transaction.commit();
         } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             e.printStackTrace();
         }
         return users;
     }
 
+    @Override
     public void cleanUsersTable() {
+        Transaction transaction = null;
         SessionFactory sessionFactory = getSessionFactory();
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
+            transaction = session.beginTransaction();
             session.createQuery("DELETE FROM User").executeUpdate();
             transaction.commit();
             System.out.println("Users table cleaned successfully");
         } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             e.printStackTrace();
         }
     }
